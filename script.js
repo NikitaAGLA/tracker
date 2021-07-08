832c2a101714c062ce8a2cbc7e4c0aa8f5811032
Nikita Gladkov <nikitagladkov@MacBook-Air-Nikita.local>
2021-07-08 19:48:53 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const newInput = document.getElementById('inputTable');
+const txtBlock = document.getElementById('textBlock');
+const buttonForAdd = document.getElementById('btn');
+//const ulLi = document.querySelector('#list ul')
+
+buttonForAdd.addEventListener('click', addNewText);
+
+function addNewText () {
+    console.log(newInput.value)
+    // if (newInput.value) {
+    //    let newText = createElement('li');
+
+    //    newText.innerText = newInput.value;
+    //    document.ul.prepend(newText);
+    
+    }
