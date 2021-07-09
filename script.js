a6ed2d6beb08c30a91aacd840a26ff3b5ef87977
Nikita Gladkov <nikitagladkov@MacBook-Air-Nikita.local>
2021-07-09 17:47:42 +0300

    js

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-const newInput = document.getElementById('inputTable');
-const txtBlock = document.getElementById('textBlock');
-const buttonForAdd = document.getElementById('btn');
-//const ulLi = document.querySelector('#list ul')
+const form = document.querySelector('form');
 
-buttonForAdd.addEventListener('click', addNewText);
-
-function addNewText () {
-    console.log(newInput.value)
-    // if (newInput.value) {
-    //    let newText = createElement('li');
-
-    //    newText.innerText = newInput.value;
-    //    document.ul.prepend(newText);
-    
+form.onsubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    if (data.get('inputTxt')) {
+        let newText = document.createElement('li');
+        newText.innerText = data.get('inputTxt');
+        console.log(data.get('inputTxt'));
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const newInput = document.querySelector('#inputTable input');
+// const txtBlock = document.getElementById('textBlock');
+// const buttonForAdd = document.querySelector('#btn button');
+// const ulForAdd = document.getElementById('ulList');
+
+// buttonForAdd.addEventListener('click', addNewText);
+
+// function addNewText () {
+    
+//     if (newInput.value) {
+//        let newText = document.createElement('li');
+
+//        newText.innerText = newInput.value;
+//        document.ulForAdd.prepend(newText);
+    
+//     }
+// }
